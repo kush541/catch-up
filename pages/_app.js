135279cad3dd775bf2e9b43ec3fb213ea7df188c
kush54 <115143746+kush54@users.noreply.github.com>
2024-06-07 03:06:14 -0700

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -3,3 +3,4 @@ import '@/styles/globals.css'
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
+// git remote add origin https://github.com/kush541/catch-up.git
